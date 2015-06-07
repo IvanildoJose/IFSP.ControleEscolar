088ae893b196af05e3ef5cbd5912d2a23826600a
henrique <henrique.contato@outlook.com>
2015-06-06 23:34:40 -0300

    CursoDisciplinaDao OK

--- IFSP.ControleEscolar/IFSP.ControleEscolar.ClassLibrary.Core/DataAccessObjects/CursoDisciplinaDao.cs
+++ IFSP.ControleEscolar/IFSP.ControleEscolar.ClassLibrary.Core/DataAccessObjects/CursoDisciplinaDao.cs
@@ -15,5 +15,64 @@ namespace IFSP.ControleEscolar.ClassLibrary.Core.DataAccessObjects
 {
     public class CursoDisciplinaDao : IDao<CursoDisciplina>
     {
+        public void Inserir(CursoDisciplina obj)
+        {
+            MySqlCommand comando = new MySqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText =
+                "INSERT INTO tbl_curso_has_tbl_disciplina(tbl_curso_cur_id, tbl_disciplina_dis_id) " +
+                "VALUES (@tbl_curso_cur_id, @tbl_disciplina_dis_id)";
+
+            comando.Parameters.AddWithValue("@tbl_curso_cur_id", obj.CursoAtrib.Id);
+            comando.Parameters.AddWithValue("@tbl_disciplina_dis_id", obj.DisciplinaAtrib.Id);
+
+            MysqlConexao.ComandoCRUD(comando);
+        }
+
+        public void Alterar(CursoDisciplina obj)
+        {
+            MySqlCommand comando = new MySqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText =
+                "UPDATE tbl_curso_has_tbl_disciplina " +
+                "SET tbl_curso_cur_id=@tbl_curso_cur_id, tbl_disciplina_dis_id=@tbl_disciplina_dis_id  " +
+                "WHERE  " +
+                "tbl_curso_cur_id=@tbl_curso_cur_id  " +
+                "AND  " +
+                "tbl_disciplina_dis_id=@tbl_disciplina_dis_id ";
+
+            comando.Parameters.AddWithValue("@tbl_curso_cur_id", obj.CursoAtrib.Id);
+            comando.Parameters.AddWithValue("@tbl_disciplina_dis_id", obj.DisciplinaAtrib.Id);
+
+            MysqlConexao.ComandoCRUD(comando);
+        }
+
+        public void Deletar(CursoDisciplina obj)
+        {
+            MySqlCommand comando = new MySqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText =
+                "DELETE FROM tbl_curso_has_tbl_disciplina " +
+                "WHERE " +
+                "WHERE  " +
+                "tbl_curso_cur_id=@tbl_curso_cur_id  " +
+                "AND  " +
+                "tbl_disciplina_dis_id=@tbl_disciplina_dis_id ";
+
+            comando.Parameters.AddWithValue("@tbl_curso_cur_id", obj.CursoAtrib.Id);
+            comando.Parameters.AddWithValue("@tbl_disciplina_dis_id", obj.DisciplinaAtrib.Id);
+
+            MysqlConexao.ComandoCRUD(comando);
+        }
+
+        public CursoDisciplina BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CursoDisciplina> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
